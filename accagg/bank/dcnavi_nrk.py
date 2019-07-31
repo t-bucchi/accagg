cde22a12d32f63395f033b3ba5d61bc18fe73c9c
bucchi <bucchi79@gmail.com>
2019-07-31 10:47:54 +0900

    [bank] Support DCnavi NRK as fund

--- accagg/bank/dcnavi_nrk.py
+++ accagg/bank/dcnavi_nrk.py
@@ -46,23 +46,31 @@ class Aggregator(Aggregator):
         return {'USRID': 'ユーザID',
                 'PASSWORD': '暗証番号'}
 
-    def _decode_date(self, str):
-        match = re.match(r"^(\d{4})/(\d+)$", str)
+    def __decode_date(self, str):
+        match = re.match(r"(\d{4})/(\d+)/(\d+)", str)
         if match:
             y = int(match.group(1))
             m = int(match.group(2))
-            d = 1
+            d = int(match.group(3))
             return date(y, m, d)
 
-    def _decode_amount(self, str):
+    def __decode_amount(self, str):
         return int('0' + str.replace(',', '').replace('円', ''))
+
+    def __fixupname(self, name):
+        if name[-3:] == '・野村':
+            name = name[:-3]
+        if name == 'ＤＣ日本株式インデックスＬ':
+            name = 'ＤＣ日本株式インデックスファンドＬ'
+        return name
 
     def run(self, login_info):
         URL = "https://www.j-pec.co.jp/dcnavi/u010login/g00101.do"
 
         profile = FirefoxProfile()
         profile.set_preference('general.useragent.override', 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko')
-        profile.set_preference("permissions.default.image", 2)
+        profile.set_preference('permissions.default.image', 2)
+        profile.set_preference('gfx.downloadable_fonts.enabled', False)
 
         browser = Browser(webdriver.Firefox(firefox_profile=profile))
         browser.implicitly_wait(180)
@@ -71,69 +79,83 @@ class Aggregator(Aggregator):
         browser.get(URL)
         browser.wait_for_loaded()
 
-#        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
 
-        browser.find_element_by_id('loginSelectLinkNRK').click();
+        browser.find_element_by_id('dcnvToNrkLogin').click();
 
         # enter
         browser.sync_send_keys((By.ID, 'loginAccount'), login_info['USRID'])
         browser.sync_send_keys((By.ID, 'loginPasswd'), login_info['PASSWORD'])
 
         # Click login
-        browser.wait_element((By.ID, 'nrkLoginLinkForNoAuto')).click()
+        browser.wait_element((By.ID, 'nrkLoginLinkForPcAuto')).click()
         browser.wait_for_loaded()
 
-        # check maintenance
-        browser.implicitly_wait(0)
-        try:
-            browser.find_element_by_css_selector('.col1 table:nth-of-type(1)')
-        except NoSuchElementException:
-#            print("No service")
-            browser.quit()
-            return None
-        browser.implicitly_wait(180)
+        # check already logged in
+        if '同じユーザーIDで利用されています' in browser.page_source:
+            browser.sync_send_keys((By.CSS_SELECTOR, 'input[name="userId"]'), login_info['USRID'])
+            browser.sync_send_keys((By.CSS_SELECTOR, 'input[name="password"]'), login_info['PASSWORD'])
+            browser.find_element_by_css_selector('input[type="SUBMIT"]').click()
+            browser.wait_for_loaded()
 
         # トップ
-        browser.find_element_by_css_selector('.col1 table:nth-of-type(1)')
-        total = browser.find_element_by_css_selector('.col1 table:nth-of-type(1) tr:nth-of-type(1) .txtR').text
-        total = self._decode_amount(total)
-
-        # 過去ページヘ
-        browser.find_elements_by_css_selector('.linkTypeNormal a')[0].click()
+        # 表示待ち
+        browser.wait_element((By.CSS_SELECTOR, '.linkTypeNormal a'))
         browser.wait_for_loaded()
 
-        # 10年
-#        browser.find_element_by_link_text('直近10年').click()
-#        browser.wait_for_loaded()
+        browser.execute_script('$("div.rkBnrBlock").removeClass("rkBnrBlock")')
+        browser.execute_script('$("a[target=\\"_blank\\"]").removeAttr("target")')
+        browser.execute_script('$(".externalLink").removeClass("externalLink")')
 
-        html = browser.find_element_by_css_selector('script:last-child').get_attribute('innerHTML')
+        # NRK のページへ
+        browser.execute_script('$("img[alt*=\\"NRK\\"]").parent().text("NRK")')
+        browser.find_element_by_link_text('NRK').click()
 
-        resp = browser.download('https://www.j-pec.co.jp/dcnavi/u080mypagerel/g02302.do?startCode=-10').decode('utf8')
-        json_text = re.sub(r'.*Chart\({', '', resp, flags=(re.DOTALL))
-        json_text = re.sub(r'}\);.*', '', json_text, flags=(re.DOTALL))
-        json_text = re.sub(r'(\S+):', r'"\1":', json_text, flags=(re.DOTALL))
-        json_text = re.sub(r'"formatter":\s+function\(\)\s*{.*?}', '', json_text, flags=(re.DOTALL))
-        json_text = re.sub(r'//.*', '', json_text)
-        json_text = re.sub(r'\'', '"', json_text)
-        json_text = '{'+json_text+'}'
-#        print(json_text)
-        d = json.loads(json_text)
-#        print(d)
-        date = d['xAxis']['categories']
-        amount = d['series'][1]['data']
+        # ここからNRKのページ
+        browser.wait_for_loaded()
 
-#        import pdb; pdb.set_trace()
-        data = []
-        for i in range(0,len(date)-1):
-#            print(date[i], amount[i])
-            if amount[i] is None:
-                continue
-            item = {'date' : self._decode_date(date[i]),
-                    'deposit' : 0,
+        # 「資産評価額照会」ページへ
+        browser.find_element_by_partial_link_text('資産評価額照会').click()
+
+        #
+        date = self.__decode_date(browser.find_elements_by_class_name('dateInq')[0].text)
+
+        result = []
+        # 現在の商品を取得
+        es = browser.find_elements_by_class_name('infoDetailUnit_02')
+        for e in es:
+            item = e.find_elements_by_tag_name('dd')
+            cols = [i.text for i in item]
+            # 商品名,商品分類,数量,基準価額,解約価額,
+            # 資産評価額,解約時評価額,取得価額累計,損益,資産比率
+
+            # print(cols)
+
+            if cols[1] == '国内投信':
+                name = self.__fixupname(cols[0])
+                unit = name
+                balance = self.__decode_amount(cols[2])
+            else:
+                name = cols[0]
+                unit = 'Yen'
+                balance = self.__decode_amount(cols[5])
+
+            result.append({
+                'name': name,
+                'unit': unit,
+                'account': 'DC',
+                'payout': self.__decode_amount(cols[7]),
+                'lastdate': date,
+                'history': [{
+                    'date' : date,
+                    'price' : 0,
+                    'amount' : 0,
+                    'payout' : 0,
+                    'balance' : balance,
                     'desc' : '',
-                    'balance' : int(amount[i]),
-            }
-            data.append(item)
+                    }],
+            })
 
+        browser.find_element_by_link_text('ログアウト').click()
         browser.quit()
-        return {'fund': data}
+        return result
